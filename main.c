bd48db51d7c4ae22f15dfc2bcdd3b7c7d8c33c89
pbrodhead <33042032+pbrodhead@users.noreply.github.com>
2018-11-28 17:06:20 -0500

    completed tuner with proper note detection

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 #define SAM_SEC 20000
 #define SAM_SEC_FLOAT 20000.0
@@ -27,7 +28,6 @@ int main(void)
     int mean            = 0;
     double freq         = 0.0;
     int freqInt         = 0;
-    int numLoop         = 0;
 
     Clock_1_Start();
     ADC_1_Start();
@@ -73,15 +73,128 @@ int main(void)
             }
         }
         
+        /* calculate frequency with correct casting */
         freq = SAM_SEC_FLOAT / maxCorrLoc;
         freq += 0.5;
-        
         freqInt = (int) freq;
-        numLoop++;
-        //sprintf(top, "freq: %d      ", freqInt);
-        sprintf(top, "maxCorrLoc: %d      ", maxCorrLoc);
-        sprintf(bottom, "numLoop: %d", numLoop);
         
+        /* output raw frequency value */
+        sprintf(bottom, "freq: %d", freqInt);
+        
+        /* determine note from frequency, and output tuning */
+        if(fabs(55 - freq) <= 0.1)
+            sprintf(top, "A       ");
+            
+        else if((freq > 55) && (freq < 56.5))
+            sprintf(top, "sharp A ");
+            
+        else if((freq > 56.5) && (freq < 58))
+            sprintf(top, "flat A# ");
+            
+        else if(fabs(58 - freq) <= 0.1)
+            sprintf(top, "A#      ");
+            
+        else if((freq > 58) && (freq < 60))
+            sprintf(top, "sharp A#");
+            
+        else if((freq > 60) && (freq < 62))
+            sprintf(top, "flat B  ");
+            
+        else if(fabs(62 - freq) <= 0.1)
+            sprintf(top, "B       ");
+            
+        else if((freq > 62) && (freq < 63.5))
+            sprintf(top, "sharp B ");
+            
+        else if((freq > 63.5) && (freq < 65.4))
+            sprintf(top, "flat C  ");
+            
+        else if(fabs(65.4 - freq) <= 0.1)
+            sprintf(top, "C       ");
+            
+        else if((freq > 65.4) && (freq < 67.4))
+            sprintf(top, "sharp C ");
+            
+        else if((freq > 67.4) && (freq < 69.3))
+            sprintf(top, "flat C# ");
+            
+        else if(fabs(69.3 - freq) <= 0.1)
+            sprintf(top, "C#      ");
+            
+        else if((freq > 69.3) && (freq < 71.4))
+            sprintf(top, "sharp C#");
+            
+        else if((freq > 71.4) && (freq < 73.4))
+            sprintf(top, "flat D  ");
+            
+        else if(fabs(73.4 - freq) <= 0.1)
+            sprintf(top, "D       ");
+            
+        else if((freq > 73.4) && (freq < 75.6))
+            sprintf(top, "sharp D ");
+            
+        else if((freq > 75.6) && (freq < 77.8))
+            sprintf(top, "flat D# ");
+            
+        else if(fabs(77.8 - freq) <= 0.1)
+            sprintf(top, "D#      ");
+            
+        else if((freq > 77.8) && (freq < 80.1))
+            sprintf(top, "sharp D#");
+            
+        else if((freq > 80.1) && (freq < 82.4))
+            sprintf(top, "flat E  ");
+            
+        else if(fabs(82.4 - freq) <= 0.1)
+            sprintf(top, "E       ");
+            
+        else if((freq > 82.4) && (freq < 84.9))
+            sprintf(top, "sharp E ");
+            
+        else if((freq > 84.9) && (freq < 87.3))
+            sprintf(top, "flat F  ");
+            
+        else if(fabs(87.3 - freq) <= 0.1)
+            sprintf(top, "F       ");
+            
+        else if((freq > 87.3) && (freq < 89.9))
+            sprintf(top, "sharp F ");
+            
+        else if((freq > 89.9) && (freq < 92.5))
+            sprintf(top, "flat F# ");
+            
+        else if(fabs(92.5 - freq) <= 0.1)
+            sprintf(top, "F#      ");
+            
+        else if((freq > 92.5) && (freq < 95.3))
+            sprintf(top, "sharp F#");
+           
+        else if((freq > 95.3) && (freq < 98))
+            sprintf(top, "flat G  ");
+            
+        else if(fabs(98 - freq) <= 0.1)
+            sprintf(top, "G       ");
+            
+        else if((freq > 98) && (freq < 100.9))
+            sprintf(top, "sharp G ");
+            
+        else if((freq > 100.9) && (freq < 103.8))
+            sprintf(top, "flat G# ");
+            
+        else if(fabs(103.8 - freq) <= 0.1)
+            sprintf(top, "G#      ");
+            
+        else if((freq > 103.8) && (freq < 106.9))
+            sprintf(top, "sharp G#");
+            
+        else if((freq > 106.9) && (freq < 110))
+            sprintf(top, "flat A  ");
+            
+        else if(fabs(110 - freq) <= 0.1)
+            sprintf(top, "A       ");
+        
+        
+        /* write to LCD screen */
         LCD_Position(0,0);
         LCD_PrintString(top);
         LCD_Position(1,0);
